97ad25f9dd4a9f6a829866665ca2a2af0eac7749
leoscrowi <leoscrowi@gmail.com>
2025-09-25 23:23:55 +0300

    repositories added

--- src/domain/core/category.py
+++ src/domain/core/category.py
@@ -3,7 +3,7 @@ import uuid
 from django.contrib.auth.models import User
 from django.db import models
 
-class Category(models.Model):
+class CategoryEntity(models.Model):
     id : models.UUIDField = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name : models.TextField = models.TextField()
     creator : models.ForeignKey = models.ForeignKey(User, on_delete=models.CASCADE)
---
+++ src/infrastructure/orm/db/expense/repositories.py
@@ -0,0 +1,53 @@
+import uuid
+from typing import List
+
+from src.domain.core.expense import ExpenseEntity
+from src.infrastructure.contracts.repositories.exceptions import EntityDoesNotExist, EntityExists
+
+
+class ExpenseDatabaseRepository:
+
+    def get(self, expense_id: uuid.UUID) -> ExpenseEntity:
+        expense = ExpenseEntity.objects.get(id=expense_id)
+        if not expense:
+            raise EntityDoesNotExist(f'expense with id {expense_id} does not exist')
+        return ExpenseEntity(**expense)
+
+    def get_user_expenses(self, user_id: uuid.UUID) -> List[ExpenseEntity]:
+        return list(ExpenseEntity.objects.filter(creator_id=user_id))
+
+    def save(self, expense: ExpenseEntity) -> ExpenseEntity:
+        try:
+            if not expense.id:
+                expense.id = uuid.uuid4()
+                expense.save()
+                return expense
+            raise EntityExists(f'category with id: {expense.id} already exists')
+        except Exception as e:
+            raise Exception(f"Error saving category: {str(e)}")
+
+    def delete(self, expense_id: uuid.UUID):
+        try:
+            category = ExpenseEntity.objects.get(id=expense_id)
+            category.delete()
+        except ExpenseEntity.DoesNotExist:
+            raise EntityDoesNotExist(f'category with id: {expense_id} does not exist')
+        except Exception as e:
+            raise Exception(f'Error deleting category: {str(e)}')
+
+    def update(self, expense: ExpenseEntity) -> ExpenseEntity:
+        try:
+            if expense.id:
+                expense = ExpenseEntity.objects.get(id=expense.id)
+                if expense:
+                    for field, value in expense.__dict__.items():
+                        if not field.startswith('_'):
+                            setattr(expense, field, value)
+                expense.save()
+                return expense
+            else:
+                expense.id = uuid.uuid4()
+                expense.save()
+                return expense
+        except Exception as e:
+            raise Exception(f"Error updating category: {str(e)}")
