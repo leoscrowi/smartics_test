6fc018df390cf7197b0696c621beca5894c1e3ce
leoscrowi <leoscrowi@gmail.com>
2025-09-28 00:30:20 +0300

    jwt-auth added

--- src/infrastructure/api/routes/urls.py
+++ src/infrastructure/api/routes/urls.py
@@ -1,7 +1,13 @@
 from django.conf import settings
 from django.urls import include, path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+    # for my own entities
     path('', include(f'{settings.API_ROUTES}.category.urls')),
-    path('', include(f'{settings.API_ROUTES}.expense.urls'))
+    path('', include(f'{settings.API_ROUTES}.expense.urls')),
+
+    # for jwt auth
+    path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh')
 ]
