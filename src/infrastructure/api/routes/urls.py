815374dba5089dea1a94745e0af8360b52d7b056
leoscrowi <leoscrowi@gmail.com>
2025-09-28 01:09:12 +0300

    added users register

--- src/infrastructure/api/routes/urls.py
+++ src/infrastructure/api/routes/urls.py
@@ -2,10 +2,15 @@ from django.conf import settings
 from django.urls import include, path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from src.infrastructure.api.views.register import RegisterViewSet
+
 urlpatterns = [
     # for my own entities
     path('', include(f'{settings.API_ROUTES}.category.urls')),
     path('', include(f'{settings.API_ROUTES}.expense.urls')),
+
+    # for register
+    path('register', RegisterViewSet.as_view(), name='register'),
 
     # for jwt auth
     path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
